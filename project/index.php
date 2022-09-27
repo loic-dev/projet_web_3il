e3573728fa3749aa2e3c92fb928d818d44e85290
loic-dev <60699159+loic-dev@users.noreply.github.com>
2022-09-27 06:41:24 +0000

    add docker

---
+++ project/index.php
@@ -0,0 +1,12 @@
+<!-- ./php/index.php -->
+
+<html>
+    <head>
+        <title>Hello World'dedeede</title>
+    </head>
+
+    <body>
+        <?php
+            echo "Hello, Worfffffeddddddedeefdcdcdcdc";?>
+    </body>
+</html>

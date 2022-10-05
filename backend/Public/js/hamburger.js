433b036377883cb0f62e316e8c3a6793c51991b9
François Siderikoudis <francoisdks@gmail.com>
2022-10-05 11:42:10 +0200

    Enfin le menu marche

--- backend/Public/js/hamburger.js
+++ backend/Public/js/hamburger.js
@@ -1,6 +1,5 @@
 document.getElementById("hamburger_icon").addEventListener("click", ()=>{
     
     // alert("test");
-    document.getElementById("hamburger_menu").classList.toggle("expand");
     document.getElementById("hamburger_menu_nav").classList.toggle("expand");
 })
--- backend/Views/Templates/header.php
+++ backend/Views/Templates/header.php
@@ -3,7 +3,7 @@
         <p>Logo</p>
         <img id="hamburger_icon" src="../../Public/media/Hamburger_icon.svg"></img>
     </div>
-    <nav id="hamburger_menu_nav">
+    <nav id="hamburger_menu_nav" style="display:none">
         <a to="/">Ajouter une annonce</a>
         <a to="/about">Connectez-vous</a>
       </nav>

aca1b032acc1387d0db2507488b17a03bd68a8eb
François <49270560+FrancoisSiderikoudis@users.noreply.github.com>
2022-11-20 21:25:50 +0100

    Update advert search

--- data/fr/Controllers/searchAdvert.php
+++ data/fr/Controllers/searchAdvert.php
@@ -8,7 +8,7 @@ $ad = new Advert();
 
 $content = trim(file_get_contents("php://input"));
 $_POST = json_decode($content, true);
-var_dump($_POST["title"]);
+// var_dump($_POST["title"]);
 
 // if(empty($_POST["title"])) $_POST["title"] = "";
 // if(empty($_POST["canton"])) $_POST["canton"] = "";
@@ -17,7 +17,7 @@ var_dump($_POST["title"]);
 // if(empty($_POST["rubric"])) $_POST["rubric"] = "";
 // if(empty($_POST["limit"])) $_POST["limit"] = "";
 
-var_dump($_POST["title"]);
+// var_dump($_POST["title"]);
 // $_POST["category"];
 // $_POST["level"];
 // $_POST["instrument"];
@@ -29,26 +29,30 @@ $sql = "SELECT * FROM Advert where Title LIKE 'tes%'";
 Database::getPdo()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 Database::getPdo()->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
+// echo object_json_response(200, json_decode($content, true));
 
-try {
-    // Database::getPdo()->setAttribute("");
-    $request = Database::getPdo()->prepare($sql);
 
-    // $request->execute([
-    $request->execute();
-    //     $_POST["title"]
-    //     // $_POST["canton"],
-    //     // $_POST["instrument"],
-    //     // $_POST["level"],
-    //     // $_POST["rubric"],
-    //     // $_POST["limit"]
-    // ]);
+// new Advert::
 
-    echo object_json_response(200, $request->fetchAll());
-} catch (PDOException $e) {
-    echo $error;
-    $error = "Une erreur est survenue";
-}
+// try {
+//     // Database::getPdo()->setAttribute("");
+//     $request = Database::getPdo()->prepare($sql);
+
+//     // $request->execute([
+//     $request->execute();
+//     //     $_POST["title"]
+//     //     // $_POST["canton"],
+//     //     // $_POST["instrument"],
+//     //     // $_POST["level"],
+//     //     // $_POST["rubric"],
+//     //     // $_POST["limit"]
+//     // ]);
+
+//     echo object_json_response(200, $request->fetchAll());
+// } catch (PDOException $e) {
+//     echo $error;
+//     $error = "Une erreur est survenue";
+// }
 
 
 ?>
